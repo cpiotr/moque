ce4674862798c2b45fee86abb48fa572c4b5a444
c.piotre <c.piotre@gmail.com>
2018-07-24 22:59:07 +0200

    Configure JMS server to accept connections

--- src/main/java/pl/ciruk/moque/jms/MockDestination.java
+++ src/main/java/pl/ciruk/moque/jms/MockDestination.java
@@ -1,51 +1,79 @@
 package pl.ciruk.moque.jms;
 
 import org.apache.activemq.artemis.api.core.TransportConfiguration;
-import org.apache.activemq.artemis.api.jms.JMSFactoryType;
 import org.apache.activemq.artemis.core.config.Configuration;
 import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.config.impl.SecurityConfiguration;
-import org.apache.activemq.artemis.core.remoting.impl.invm.InVMAcceptorFactory;
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
-import org.apache.activemq.artemis.jms.server.JMSServerManager;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
+import org.apache.activemq.artemis.jms.server.config.ConnectionFactoryConfiguration;
+import org.apache.activemq.artemis.jms.server.config.JMSConfiguration;
+import org.apache.activemq.artemis.jms.server.config.JMSQueueConfiguration;
+import org.apache.activemq.artemis.jms.server.config.impl.ConnectionFactoryConfigurationImpl;
 import org.apache.activemq.artemis.jms.server.config.impl.JMSConfigurationImpl;
+import org.apache.activemq.artemis.jms.server.config.impl.JMSQueueConfigurationImpl;
 import org.apache.activemq.artemis.jms.server.embedded.EmbeddedJMS;
-import org.apache.activemq.artemis.spi.core.security.ActiveMQJAASSecurityManager;
-import org.apache.activemq.artemis.spi.core.security.jaas.InVMLoginModule;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
-import java.util.HashSet;
-import java.util.List;
+import java.util.function.Predicate;
 
 public class MockDestination implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
     @Override
     public void beforeTestExecution(ExtensionContext context) throws Exception {
-        Configuration config = new ConfigurationImpl();
-        HashSet<TransportConfiguration> transports = new HashSet<>();
+        Configuration configuration = new ConfigurationImpl();
+        configuration.setPersistenceEnabled(false);
+        configuration.setSecurityEnabled(false);
+        configuration.getAcceptorConfigurations().add(new TransportConfiguration(NettyAcceptorFactory.class.getName()));
+        configuration.addConnectorConfiguration(
+                NettyConnectorFactory.class.getName(),
+                new TransportConfiguration(NettyConnectorFactory.class.getName()));
 
-        transports.add(new TransportConfiguration(NettyAcceptorFactory.class.getName()));
-        transports.add(new TransportConfiguration(InVMAcceptorFactory.class.getName()));
+        JMSConfiguration jmsConfig = new JMSConfigurationImpl();
 
-        config.setAcceptorConfigurations(transports);
+        ConnectionFactoryConfiguration cfConfig = createConnectionFactoryConfiguration();
+        jmsConfig.getConnectionFactoryConfigurations().add(cfConfig);
+
+        String queue1 = "queue1";
+        JMSQueueConfiguration queueConfig = createQueueConfiguration(queue1);
+        jmsConfig.getQueueConfigurations().add(queueConfig);
 
         EmbeddedJMS jmsServer = new EmbeddedJMS();
-        jmsServer.setConfiguration(config);
-        jmsServer.setJmsConfiguration(new JMSConfigurationImpl());
+        jmsServer.setConfiguration(configuration);
+        jmsServer.setJmsConfiguration(jmsConfig);
         jmsServer.start();
 
-        ConnectionFactory connectionFactory = (ConnectionFactory) jmsServer.lookup("ConnectionFactory");
-        Destination destination = (Destination) jmsServer.lookup("/example/queue");
+        ConnectionFactory connectionFactory = (ConnectionFactory) jmsServer.lookup("/cf");
 
         context.getStore(ExtensionContext.Namespace.create("moque")).put("jmsServer", jmsServer);
+    }
+
+    @NotNull
+    private ConnectionFactoryConfiguration createConnectionFactoryConfiguration() {
+        ConnectionFactoryConfiguration cfConfig = new ConnectionFactoryConfigurationImpl();
+        cfConfig.setName("cf");
+        cfConfig.setBindings("/cf");
+        cfConfig.setConnectorNames(NettyConnectorFactory.class.getName());
+        return cfConfig;
+    }
+
+    @NotNull
+    private JMSQueueConfiguration createQueueConfiguration(String name) {
+        JMSQueueConfiguration queueConfig = new JMSQueueConfigurationImpl();
+        queueConfig.setName(name);
+        queueConfig.setName(name);
+        return queueConfig;
     }
 
     @Override
     public void afterTestExecution(ExtensionContext context) throws Exception {
         EmbeddedJMS jmsServer = context.getStore(ExtensionContext.Namespace.create("moque")).get("jmsServer", EmbeddedJMS.class);
         jmsServer.stop();
+    }
+
+    public <T> void whenReceived(String queueName, Predicate<T> messageMatcher) {
+
     }
 }
