70fcbe43060a9b0104d67cb9eeead8d42f9bd5aa
c.piotre <c.piotre@gmail.com>
2018-08-07 23:32:51 +0200

    Rename single queue test

--- src/test/java/pl/ciruk/moque/jms/JmsMoqueSingleQueueTest.java
+++ src/test/java/pl/ciruk/moque/jms/JmsMoqueSingleQueueTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
-class JmsMoqueTest {
+class JmsMoqueSingleQueueTest {
     private static final String QUEUE_NAME = "Q1";
 
     @RegisterExtension
